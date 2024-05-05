09fc63fa5fe88f3720863c536097c3228dc45c59
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-05-05 16:56:31 +0330

    debuging ...

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
+server.listen(4000, () => {
   console.log("JSON Server is running");
 });
 
