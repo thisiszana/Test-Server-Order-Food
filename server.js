9c9ddaca9e02fe2c8f6307d3e606f8704bda3031
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-05-05 18:17:30 +0330

    change server listen

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(4000, () => {
+server.listen(3000, () => {
   console.log("JSON Server is running");
 });
 
